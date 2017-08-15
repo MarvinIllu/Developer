610332d6c464d0f7db76d08797233e50c322997e
Marvin Daniel Illu Garcia <marvindaniel@hotmail.es>
2017-08-14 21:21:09 -0600

    Subida de codigo

---
+++ ControlPagosInbaco/ControlPagosInbaco/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControlPagosInbaco.MvcApplication" Language="C#" %>
---
+++ ControlPagosInbaco/ControlPagosInbaco/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControlPagosInbaco.Startup))]
+namespace ControlPagosInbaco
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
